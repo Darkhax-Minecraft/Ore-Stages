20e0c81c87262fe224fba79862836e229a87f496
Jared <jaredlll08@gmail.com>
2017-01-27 02:11:11 +0200

    Initial mod files

---
+++ src/main/java/com/jarhax/sevtechores/STO.java
@@ -0,0 +1,14 @@
+package com.jarhax.sevtechores;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.jarhax.sevtechores.STO.*;
+
+@Mod(modid = MODID, name = NAME, version = VERSION)
+public class STO {
+	
+	public static final String MODID = "sevtechores";
+	public static final String NAME = "SevTech-Ores";
+	public static final String VERSION = "1.0.0";
+	
+}
