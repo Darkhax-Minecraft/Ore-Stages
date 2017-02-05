678a700677e05355d58a32c2a803c9872e32d011
Tyler Hancock <darklime@live.ca>
2017-02-04 23:31:56 -0700

    Made the command temporarily useless.

--- src/main/java/com/jarhax/sevtechores/commands/CommandMinableInfo.java
+++ src/main/java/com/jarhax/sevtechores/commands/CommandMinableInfo.java
@@ -1,14 +1,9 @@
 package com.jarhax.sevtechores.commands;
 
-import com.jarhax.sevtechores.STO;
-import com.jarhax.sevtechores.handler.WorldGenHandler;
-
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.text.TextComponentTranslation;
 
 public class CommandMinableInfo extends CommandBase {
 
@@ -27,13 +22,5 @@ public class CommandMinableInfo extends CommandBase {
 	@Override
 	public void execute (MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
 
-		STO.LOGGER.info("Starting State Dump!");
-		for (final IBlockState state : WorldGenHandler.KNOWN_STATES) {
-
-			STO.LOGGER.info(state.toString());
-		}
-		final int amount = WorldGenHandler.KNOWN_STATES.size();
-		STO.LOGGER.info(String.format("Ending State Dump! %d entries!", amount));
-		sender.addChatMessage(new TextComponentTranslation("commands.sevores.known.size%n", new Object[] { amount }));
 	}
 }
