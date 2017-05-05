d749486865105a003e624c9a2c43f24072a64dca
Jared <jaredlll08@gmail.com>
2017-05-05 19:07:00 +0200

    Base mod files

---
+++ src/main/java/com/jarhax/sevtechores/STO.java
@@ -0,0 +1,28 @@
+package com.jarhax.sevtechores;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.*;
+import org.apache.logging.log4j.*;
+
+import static com.jarhax.sevtechores.reference.Reference.*;
+
+@Mod(modid = MODID, name = NAME, version = VERSION)
+public class STO {
+    
+    public static final Logger LOGGER = LogManager.getLogger(NAME);
+    
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent ev) {
+        
+    }
+    
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent ev) {
+    
+    }
+    
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent ev) {
+    
+    }
+}
---
+++ src/main/java/com/jarhax/sevtechores/reference/Reference.java
@@ -0,0 +1,7 @@
+package com.jarhax.sevtechores.reference;
+
+public class Reference {
+    public static final String MODID = "sevtechores";
+    public static final String NAME = "SevTech-Ores";
+    public static final String VERSION = "1.0.0.0";
+}
