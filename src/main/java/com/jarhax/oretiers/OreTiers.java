99672998a95a61bce95938581f5625b91d20f99f
Tyler Hancock <darklime@live.ca>
2017-05-06 10:17:07 -0600

    Fixed formatting.

--- src/main/java/com/jarhax/oretiers/OreTiers.java
+++ src/main/java/com/jarhax/oretiers/OreTiers.java
@@ -10,19 +10,19 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
                                                                     // =
                                                                     // "required-after:bookshelf@[@VERSION_BOOKSHELF@,);required-after:minetweaker3@[@VERSION_MINETWEAKER@,)")
 public class OreTiers {
-    
+
     @Mod.EventHandler
     public void preInit (FMLPreInitializationEvent ev) {
-        
+
     }
-    
+
     @Mod.EventHandler
     public void init (FMLInitializationEvent ev) {
-        
+
     }
-    
+
     @Mod.EventHandler
     public void postInit (FMLPostInitializationEvent ev) {
-        
+
     }
 }
