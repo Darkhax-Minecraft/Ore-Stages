67878d04d97a2fa7fcb8fc94d3944c9b6f271c08
Jared <jaredlll08@gmail.com>
2017-05-07 20:43:38 +0200

    Fixed Proxy String

--- src/main/java/com/jarhax/oretiers/OreTiers.java
+++ src/main/java/com/jarhax/oretiers/OreTiers.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = "oretiers", name = "Ore Tiers", version = "@VERSION@", dependencies = "required-after:bookshelf@[1.5.0.370,);required-after:crafttweaker@[3.0.23,)")
 public class OreTiers {
 
-    @SidedProxy(clientSide = "com.jarhax.proxy.ProxyClient", serverSide = "com.jarhax.proxy.ProxyCommon")
+    @SidedProxy(clientSide = "com.jarhax.oretiers.proxy.ProxyClient", serverSide = "com.jarhax.oretiers.proxy.ProxyCommon")
     public static ProxyCommon proxy;
 
     public static NetworkHandler network = new NetworkHandler("oretiers");
