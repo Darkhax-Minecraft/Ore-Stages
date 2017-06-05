8cae36df2361721252ed4d1013cb64f29ce12d1e
Tyler Hancock <darklime@live.ca>
2017-06-05 14:31:49 -0600

    Removed packets. They're nolonger needed.

--- src/main/java/com/jarhax/oretiers/OreTiers.java
+++ src/main/java/com/jarhax/oretiers/OreTiers.java
@@ -2,38 +2,25 @@ package com.jarhax.oretiers;
 
 import org.apache.logging.log4j.Logger;
 
-import com.jarhax.oretiers.api.OreTiersAPI;
 import com.jarhax.oretiers.api.StageDataHandler;
 import com.jarhax.oretiers.compat.crt.OreTiersCrT;
-import com.jarhax.oretiers.packet.PacketReloadModels;
-import com.jarhax.oretiers.packet.PacketRequestClientRefresh;
 
-import net.darkhax.bookshelf.network.NetworkHandler;
 import net.darkhax.gamestages.capabilities.PlayerDataHandler;
-import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = "oretiers", name = "Ore Tiers", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.0.0.380,);required-after:crafttweaker@[3.0.25.,)")
 public class OreTiers {
 
     public static Logger log;
-    public static NetworkHandler NETWORK = new NetworkHandler("oretiers");
 
     @Mod.EventHandler
     public void preInit (FMLPreInitializationEvent ev) {
 
-        NETWORK.register(PacketReloadModels.class, Side.CLIENT);
-        NETWORK.register(PacketRequestClientRefresh.class, Side.CLIENT);
-
         log = ev.getModLog();
         MinecraftForge.EVENT_BUS.register(new OreTiersEventHandler());
-
-        OreTiersAPI.addReplacement("one", Blocks.IRON_ORE, Blocks.STONE);
-        OreTiersAPI.addReplacement("two", Blocks.GOLD_ORE, Blocks.STONE);
         PlayerDataHandler.registerDataHandler("oretiers", new StageDataHandler());
     }
 
