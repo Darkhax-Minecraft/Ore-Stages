c227c511d0ae923f775386c0369a400f051110e6
Tyler Hancock <darklime@live.ca>
2017-08-05 17:33:00 -0600

    Removed unused CrT register call.

--- src/main/java/com/jarhax/oretiers/OreTiers.java
+++ src/main/java/com/jarhax/oretiers/OreTiers.java
@@ -3,14 +3,12 @@ package com.jarhax.oretiers;
 import org.apache.logging.log4j.Logger;
 
 import com.jarhax.oretiers.api.StageDataHandler;
-import com.jarhax.oretiers.compat.crt.OreTiersCrT;
 
 import net.darkhax.gamestages.capabilities.PlayerDataHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "oretiers", name = "Ore Tiers", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.0.0.380,);required-after:crafttweaker@[3.0.25.,)")
@@ -29,11 +27,5 @@ public class OreTiers {
 
             FMLInterModComms.sendMessage("waila", "register", "com.jarhax.oretiers.compat.waila.OreTiersProvider.register");
         }
-    }
-
-    @Mod.EventHandler
-    public void postInit (FMLPostInitializationEvent ev) {
-
-        OreTiersCrT.init();
     }
 }
